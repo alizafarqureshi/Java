a3867125f2c51192ba7554ea4436ec141cda1c7b
alizafarqureshi <80305341+alizafarqureshi@users.noreply.github.com>
2022-03-17 01:47:28 +0500

    Add files via upload

---
+++ DSAExam/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StackDS st = new StackDS(5);
+        st.push(2);
+        st.push(23;
+
+        st.pop();
+
+        st.evenNumbers();
+
+
+
+        st.powerOfNumber();
+
+
+
+        st.print();
+
+
+
+
+
+    }
+}
